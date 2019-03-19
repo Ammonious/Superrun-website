3328605691c2f1ec6a4f95d6b38d7f30ef7b6a36
Ammon Rees <ammonrees@ammons-imac.lan>
2019-03-19 12:12:34 -0600

    Registration update

--- public/js/registration.js
+++ public/js/registration.js
@@ -18,32 +18,87 @@ window.onload = function () {
 	
 	
 	
+	var schoolOptions = document.getElementById('schoolSelection');
+	var schoolRef = db.collection("Schools");
+	schoolRef.get().then(function(querySnapshot) {
+		querySnapshot.forEach(function(doc) {
+			console.log("Document data:", doc.data());
+			var data = doc.data();
+			var schoolName = data.School;
+			var myJSON = JSON.stringify(data);
+			console.log("School Name: ", schoolName);
+			console.log(myJSON);
+			var option = document.createElement("option");
+			option.text = schoolName;
+			option.value = myJSON;
+			schoolOptions.options.add(option);
+		//	var schoolName = doc.data().
+		});
+		 
+	})
 	
+var teacherOptions = document.getElementById('teacherSelection');
+schoolOptions.onchange = function(){
+	console.log("On Select school called");
+	var x = document.getElementById("schoolSelection").selectedIndex;
+	var school = document.getElementsByTagName("option")[x].value;
+	var obj = JSON.parse(school); 
 	
+	var schoolId = obj.documentId;
+	var ref = db.collection("Users");
+	console.log('school id ' + schoolId);
+	var query = ref.where("schoolId", "==",schoolId).where("title","==","Teacher");
 	
+	query.get().then(function(querySnapshot) {
+		querySnapshot.forEach(function(doc) {
+			console.log("Document data:", doc.data());
+			var data = doc.data();
+			var teacherName = data.name;
+			
+			console.log("Teacher Name: ", teacherName);
+			var option = document.createElement("option");
+			option.text = teacherName;
+			option.value = JSON.stringify(data);
+			teacherOptions.options.add(option);
+		//	var schoolName = doc.data().
+		});
+	})
+}
+var teacherJSON;
+teacherOptions.onchange = function() {
 	
+	var x = teacherOptions.selectedIndex;
+	var teacherjson = document.getElementsByTagName("option")[1].value;
+	var teacher = JSON.parse(teacherjson);
+	console.log(teacherjson);
 	
-document.getElementById("pledgeBtn").onclick = function () {
+}
+	
+document.getElementById("submit").onclick = function () {
 	
 		 var name = document.getElementById('name');
 		 var email = document.getElementById('email');
 		 var phone = document.getElementById('phone'); 
-		 var schoolName = document.getElementById("schoolSelection").options[document.getElementById("schoolSelection").selectedIndex].value;
+		 var schoolName = document.getElementById("schoolSelection").options[document.getElementById("schoolSelection").selectedIndex].text;
 		 
 		 var x = document.getElementById("schoolSelection").selectedIndex;
-		 var school = document.getElementsByTagName("option")[x].value;
+		 var json = document.getElementsByTagName("option")[x].value;
 		 var school = JSON.parse(json);
-		
+		 
 	var student = {
-				name :  name,
-				email : email,
-				phone : phone,
-				schoolName : schoolName,
+				'name' :  name,
+				'email' : email,
+				'phone' : phone,
+				'schoolName' : schoolName,
+				'schoolId' : school.documentId,
+				'school' : school,
+				'teacher' : teacher,
+				'teacherId' : teacher.uid,
 		}
 		
 		
 	axios.post('https://us-central1-funrun-dd997.cloudfunctions.net/registerStudent', student).then(function (res) { 
-					 // Navigate to Registered Screen.
+		// Navigate to Registered Screen.
 					 
 				  
 	}).catch(function (err) {
@@ -53,9 +108,6 @@ document.getElementById("pledgeBtn").onclick = function () {
 
 
 }
-
-
-
 			
 
 };
